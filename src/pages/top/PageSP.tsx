45e30d84926a3b30f3312210068016ac63306333
Dongwon <dongwon@DongwonnoMacBook-Pro.local>
2024-12-25 04:45:20 +0900

    mobile - image quality 75

--- src/pages/top/PageSP.tsx
+++ src/pages/top/PageSP.tsx
@@ -12,7 +12,7 @@ export default function PageSP() {
         height={300}
         alt='DongwonTTuna'
         className={CSS.main__image}
-        quality={100}
+        quality={75}
       />
     </main>
   )
