f1de10e1eda81417af80c55dbeeabc328e11eac2
Dongwon <dongwon@DongwonnoMacBook-Pro.local>
2024-12-25 02:24:25 +0900

    SPページのデプロイ

--- src/pages/top/PageSP.tsx
+++ src/pages/top/PageSP.tsx
@@ -1,3 +1,11 @@
+import Image from 'next/image'
+import TunaTopImage from './_images/tuna_top.png'
+import './_css/pc_page.css'
+
 export default function PageSP() {
-  return <div>SP</div>
+  return (
+    <main className='dongwon'>
+      <Image src={TunaTopImage} width={300} height={300} alt='DongwonTTuna' className={'main__image'} />
+    </main>
+  )
 }
