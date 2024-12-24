eb85aab86de43582561980421bf56be53d405dbe
Dongwon <dongwon@DongwonnoMacBook-Pro.local>
2024-12-25 00:58:29 +0900

    Add Css animation on PC-top

--- src/pages/top/PagePC.tsx
+++ src/pages/top/PagePC.tsx
@@ -1,3 +1,11 @@
+import Image from 'next/image'
+import TunaTopImage from './_images/tuna_top.png'
+import './_css/pc_page.css'
+
 export default function PagePC() {
-  return <div>PC</div>
+  return (
+    <main className='dongwon'>
+      <Image src={TunaTopImage} width={500} height={500} alt='DongwonTTuna' className={'main__image'} />
+    </main>
+  )
 }
