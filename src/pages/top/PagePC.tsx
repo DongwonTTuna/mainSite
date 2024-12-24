b58916b91bdd48377a451f63c46f2779881619a6
Dongwon <dongwon@DongwonnoMacBook-Pro.local>
2024-12-25 04:58:37 +0900

    unpicを追加

--- src/pages/top/PagePC.tsx
+++ src/pages/top/PagePC.tsx
@@ -1,18 +1,17 @@
-import Image from 'next/image'
-import TunaTopImage from './_images/tuna_top.png'
+import { Image } from '@unpic/react/nextjs'
 import CSS from './_css/pc_page.module.css'
 
 export default function PagePC() {
   return (
     <main className={CSS.main__container}>
       <Image
-        className={CSS.main__image}
-        src={TunaTopImage}
+        src='https://res.cloudinary.com/dfd9pdmuf/image/upload/v1735070084/dongwontuna/ap59sakylhpoklaxsfkn.png'
         loading={'eager'}
         width={500}
         height={500}
         alt='DongwonTTuna'
-        quality={100}
+        layout={'fixed'}
+        className={CSS.main__image}
       />
     </main>
   )
--- src/pages/top/PageSP.tsx
+++ src/pages/top/PageSP.tsx
@@ -1,18 +1,17 @@
-import Image from 'next/image'
-import TunaTopImage from './_images/tuna_top.png'
+import { Image } from '@unpic/react/nextjs'
 import CSS from './_css/sp_page.module.css'
 
 export default function PageSP() {
   return (
     <main className={CSS.main__container}>
       <Image
-        src={TunaTopImage}
+        src='https://res.cloudinary.com/dfd9pdmuf/image/upload/v1735070084/dongwontuna/ap59sakylhpoklaxsfkn.png'
         loading={'eager'}
         width={300}
         height={300}
         alt='DongwonTTuna'
+        layout={'fixed'}
         className={CSS.main__image}
-        quality={75}
       />
     </main>
   )
