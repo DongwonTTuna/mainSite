bf3700a1d3f0fd7a5360207b1cd92b738c7c1f98
Dongwon <dongwon@DongwonnoMacBook-Pro.local>
2024-12-26 01:51:59 +0900

    remove edge

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,5 +6,3 @@ import type { AppProps } from 'next/app'
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-
-export const runtime = 'edge'
