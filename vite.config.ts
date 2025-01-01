efe365564b52050b02930455f103f97616091353
Dongwon <dongwon@DongwonnoMacBook-Pro.local>
2025-01-01 19:03:02 +0900

    init commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config';
+import { paraglide } from '@inlang/paraglide-sveltekit/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+export default defineConfig({
+	plugins: [
+		sveltekit(),
+		paraglide({
+			project: './project.inlang',
+			outdir: './src/lib/i18n/generated'
+		})
+	],
+
+	test: {
+		include: ['src/__tests__/unit/*.{test,spec}.{js,ts}']
+	}
+});
