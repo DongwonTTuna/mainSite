066a3ddb05d44c4e145d2002b4cae6784e5e4e3b
DongwonTTuna <ttuna0790@gmail.com>
2025-07-20 13:28:30 +0900

    SSGに変更

--- src/lib/hooks/server/hooks.server.ts
+++ src/lib/hooks/server/hooks.server.ts
@@ -1,5 +1,4 @@
 import { sequence } from "@sveltejs/kit/hooks"
-import { deviceDetectionHandler } from "$lib/hooks/server/device-detection-handler"
 import { handleParaglide } from "$lib/hooks/server/i18n-handler"
 
-export const handle = sequence(deviceDetectionHandler, handleParaglide)
+export const handle = sequence(handleParaglide)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,7 @@
 <script lang="ts">
   import type { PageData } from "./$types"
-  import PageSP from "$views/pages/top/PageSP.svelte"
-  import PagePC from "$views/pages/top/PagePC.svelte"
-  import Layout from "$views/common/layout/Layout.svelte"
 
   const props = $props()
   const data = props.data as PageData
 </script>
 
-<Layout isMobile={data.isMobile}>
-  {#if data.isMobile}
-    <PageSP />
-  {:else}
-    <PagePC />
-  {/if}
-</Layout>
